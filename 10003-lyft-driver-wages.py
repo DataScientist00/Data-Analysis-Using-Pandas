524896c64e606353a90f118c807130060650949a
DataScientist00 <nikunjmishra00@gmail.com>
2024-09-06 08:09:20 +0530

    Create 10003-lyft-driver-wages.py

---
+++ 10003-lyft-driver-wages.py
@@ -0,0 +1,8 @@
+#problem link-->>> https://platform.stratascratch.com/coding/10003-lyft-driver-wages?code_type=2
+
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+lyft_drivers[(lyft_drivers['yearly_salary'] <= 30000) | (lyft_drivers['yearly_salary'] >= 70000 )]
