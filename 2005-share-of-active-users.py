86d36f8f5e9e0617929ea48edd2fca82ceae6040
DataScientist00 <nikunjmishra00@gmail.com>
2024-11-20 08:41:08 +0530

    Create 2005-share-of-active-users.py

---
+++ 2005-share-of-active-users.py
@@ -0,0 +1,9 @@
+#problem link-->> https://platform.stratascratch.com/coding/2005-share-of-active-users?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+df = len(fb_active_users.query('status=="open" and country =="USA"')) / len(fb_active_users.query('country=="USA"'))
+df1 = {'open':[df]}
+pd.DataFrame(df1)
