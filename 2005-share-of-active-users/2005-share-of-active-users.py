e16194482065bee3fa04f72800cf0c02486d5d54
DataScientist00 <nikunjmishra00@gmail.com>
2024-11-20 08:40:21 +0530

    Delete 2005-share-of-active-users directory

--- 2005-share-of-active-users/2005-share-of-active-users.py
+++
@@ -1,9 +0,0 @@
-#problem link-->> https://platform.stratascratch.com/coding/2005-share-of-active-users/solutions?code_type=2
-
-# Import your libraries
-import pandas as pd
-
-# Start writing code
-df = len(fb_active_users.query('status=="open" and country =="USA"')) / len(fb_active_users.query('country=="USA"'))
-df1 = {'open':[df]}
-pd.DataFrame(df1)
