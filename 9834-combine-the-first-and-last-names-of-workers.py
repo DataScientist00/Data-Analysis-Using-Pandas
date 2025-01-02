c5b05b9a19d39192a627595b6281c109f68258e4
DataScientist00 <nikunjmishra00@gmail.com>
2025-01-02 08:35:54 +0530

    Create 9834-combine-the-first-and-last-names-of-workers.py

---
+++ 9834-combine-the-first-and-last-names-of-workers.py
@@ -0,0 +1,8 @@
+#problem link-->> https://platform.stratascratch.com/coding/9834-combine-the-first-and-last-names-of-workers?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+worker['full_name'] = worker['first_name'] + ' '+ worker['last_name']
+worker['full_name']
