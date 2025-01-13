54a849e884bc81ff0219c8a2a2e2f2c175827b5b
DataScientist00 <nikunjmishra00@gmail.com>
2025-01-13 07:49:04 +0530

    Create 9768-find-all-posts-with-the-keyword-nba.py

---
+++ 9768-find-all-posts-with-the-keyword-nba.py
@@ -0,0 +1,7 @@
+#problem link-->> https://platform.stratascratch.com/coding/9768-find-all-posts-with-the-keyword-nba?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+facebook_posts[facebook_posts['post_keywords'].str.contains('nba')]
