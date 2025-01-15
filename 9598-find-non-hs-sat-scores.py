ce7db1cf25d1a9ac58f6e909f2ade77b10375dcf
DataScientist00 <nikunjmishra00@gmail.com>
2025-01-15 08:16:05 +0530

    Create 9598-find-non-hs-sat-scores.py

---
+++ 9598-find-non-hs-sat-scores.py
@@ -0,0 +1,7 @@
+#problem link-->> https://platform.stratascratch.com/coding/9598-find-non-hs-sat-scores?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+sat_scores[~sat_scores['school'].str.endswith('HS')]
