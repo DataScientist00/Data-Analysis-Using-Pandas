9f030a25beca84b47d58942487842414e9624763
DataScientist00 <nikunjmishra00@gmail.com>
2025-01-23 08:55:01 +0530

    Create 9828-print-the-first-three-characters-of-the-first-name.py

---
+++ 9828-print-the-first-three-characters-of-the-first-name.py
@@ -0,0 +1,7 @@
+#problem link-->> https://platform.stratascratch.com/coding/9828-print-the-first-three-characters-of-the-first-name?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+worker['first_name'].str[:3]
