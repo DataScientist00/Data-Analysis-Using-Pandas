a086bd32d104dd95fedb4f5dbd935b700a85d215
DataScientist00 <nikunjmishra00@gmail.com>
2025-02-04 07:45:41 +0530

    Create 9829-positions-of-letter-a.py

---
+++ 9829-positions-of-letter-a.py
@@ -0,0 +1,7 @@
+#problem link-->> https://platform.stratascratch.com/coding/9829-positions-of-letter-a?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+worker.query("first_name =='Amitah'")['first_name'].str.find(sub='a')
